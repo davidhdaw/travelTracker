0b8b00b8ae1036f8a8d01d9db8e0c0321b430221
David Daw <daviddaw@gmail.com>
2022-06-10 15:34:29 -0700

    Add Destination and Trip classes that pass all tests

--- src/Destinations.js
+++ src/Destinations.js
@@ -1,7 +1,16 @@
 class Destinations {
-  constructor() {
-    
+  constructor(data) {
+    this.destinations = data
+  };
+  findDestination(destinationID) {
+    return this.destinations.find(destination => destination.id === destinationID)
+  };
+  findTripCost(destinationID, travelersNum, tripLength) {
+    const destinationInfo = this.findDestination(destinationID);
+    const flightCost = travelersNum * destinationInfo.estimatedFlightCostPerPerson
+    const lodgingCost = travelersNum * tripLength * destinationInfo.estimatedLodgingCostPerDay
+    return flightCost + lodgingCost 
   }
-}
+};
 
 export default Destinations;
