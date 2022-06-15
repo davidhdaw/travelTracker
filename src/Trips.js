a9795f1461f927001fa08bc6f0a5370a94fe67fc
David Daw <daviddaw@gmail.com>
2022-06-14 17:08:27 -0700

    Refactor javascript

--- src/Trips.js
+++ src/Trips.js
@@ -1,47 +1,47 @@
 class Trips {
   constructor(tripsData) {
-    this.trips = tripsData.trips;
-  };
+    this.trips = tripsData.trips
+  }
 
   findUserTrips(userID) {
-    return this.trips.filter(trip => trip.userID === userID);
-  };
+    return this.trips.filter(trip => trip.userID === userID)
+  }
 
   findPendingTrips(userID) {
-    return this.findUserTrips(userID).filter(trip => trip.status === 'pending');
+    return this.findUserTrips(userID).filter(trip => trip.status === 'pending')
   }
 
   findFutureTrips(userID, date) {
     let tripsWithToday = this.findUserTrips(userID)
     tripsWithToday.push({"id":0,"userID":0,"destinationID":0,"travelers":0,"date":date,"duration":0})
-    let orderedTrips = tripsWithToday.sort((a,b) => a.date > b.date ? 1 : -1);
+    let orderedTrips = tripsWithToday.sort((a,b) => a.date > b.date ? 1 : -1)
     const dateIndex = orderedTrips.findIndex(trip => trip.date === date)
-    return orderedTrips.slice(dateIndex+1);
+    return orderedTrips.slice(dateIndex+1)
   }
 
   findPastTrips(userID, date) {
     let tripsWithToday = this.findUserTrips(userID)
     tripsWithToday.push({"id":0,"userID":0,"destinationID":0,"travelers":0,"date":date,"duration":0})
-    let orderedTrips = tripsWithToday.sort((a,b) => a.date > b.date ? 1 : -1);
+    let orderedTrips = tripsWithToday.sort((a,b) => a.date > b.date ? 1 : -1)
     const dateIndex = orderedTrips.findIndex(trip => trip.date === date)
-    return orderedTrips.slice(0, dateIndex);
+    return orderedTrips.slice(0, dateIndex)
   }
 
   findNextTrip(userID, date) {
     let tripsWithToday = this.findUserTrips(userID)
     tripsWithToday.push({"id":0,"userID":0,"destinationID":0,"travelers":0,"date":date,"duration":0})
-    let orderedTrips = tripsWithToday.sort((a,b) => a.date > b.date ? 1 : -1);
+    let orderedTrips = tripsWithToday.sort((a,b) => a.date > b.date ? 1 : -1)
     const dateIndex = orderedTrips.findIndex(trip => trip.date === date)
-    return orderedTrips[dateIndex+1];
-  };
+    return orderedTrips[dateIndex+1]
+  }
 
     findLastTrip(userID, date) {
       let tripsWithToday = this.findUserTrips(userID)
       tripsWithToday.push({"id":0,"userID":0,"destinationID":0,"travelers":0,"date":date,"duration":0})
-      let orderedTrips = tripsWithToday.sort((a,b) => a.date > b.date ? 1 : -1);
+      let orderedTrips = tripsWithToday.sort((a,b) => a.date > b.date ? 1 : -1)
       const dateIndex = orderedTrips.findIndex(trip => trip.date === date)
-      return orderedTrips[dateIndex-1];
-    };
-};
+      return orderedTrips[dateIndex-1]
+    }
+}
 
-export default Trips;
+export default Trips
